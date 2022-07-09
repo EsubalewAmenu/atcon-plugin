a382c23a2252faf2a21664d19de48ef2184bfabf
Esubalew Desktop <esubalew.a2009@gmail.com>
2022-07-09 13:29:07 +0300

    first commit

---
+++ includes/class-atcon-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/EsubalewAmenu/
+ * @since      1.0.0
+ *
+ * @package    Atcon
+ * @subpackage Atcon/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Atcon
+ * @subpackage Atcon/includes
+ * @author     Esubalew Amenu <esubalew.a2009@gmail.com>
+ */
+class Atcon_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
